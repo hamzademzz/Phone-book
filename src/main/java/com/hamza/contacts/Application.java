81a2a750c71bf0e64c9d8e501927769e924ffaf6
Hamza Demir <hamzademir4772@hotmail.com>
2018-11-22 21:43:36 +0000

    Add Contacts

--- src/main/java/com/hamza/contacts/Application.java
+++ src/main/java/com/hamza/contacts/Application.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 public class Application{
     public static void main(String[] args){
 
+
+
+
         ask();
 
+
     }
+
+    public static Contact[] contacts=new Contact[];
 
     public static void ask(){
         System.out.println("Please select an option");
@@ -28,4 +34,41 @@ public class Application{
     public static void list(){
         System.out.println("List of Contacts");
     }
+
+    public static void addContact(){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter First Name");
+        String firstName=input.next();
+        System.out.println("Enter Last Name");
+        String lastName=input.next();
+        System.out.println("Enter Company");
+        String company=input.next();
+        System.out.println("Enter Phone Number");
+        String phoneNumber=input.next();
+        System.out.println("Enter Email");
+        String email=input.next();
+        System.out.println("Enter URL ");
+        String url=input.next();
+        System.out.println("Enter Address");
+        String address=input.next();
+        System.out.println("Enter birthday");
+        String birthday=input.next();
+
+        Contact contact=new Contact();
+        contact.setFirstName(firstName);
+        contact.setLastName(lastName);
+        contact.setCompany(company);
+        contact.setPhoneNumber(phoneNumber);
+        contact.setEmail(email);
+        contact.setUrl(url);
+        contact.setAddress(address);
+        contact.setBirthday(birthday);
+        contacts[0]=contact;
+
+    }
+
+
+
+
+
 }
--- src/main/java/com/hamza/contacts/Contact.java
+++ src/main/java/com/hamza/contacts/Contact.java
@@ -1,15 +1,76 @@
 package com.hamza.contacts;
 
 public class Contact {
+   private String firstName;
+   private String lastName;
+   private String company;
+   private String phoneNumber;
+   private String email;
+   private String url;
+   private String address;
+   private String birthday;
 
-    String firstName;
-    String lastName;
-    String company;
-    String phone;
-    String email;
-    String url;
-    String address;
-    String birthday;
-    String date;
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
 }
