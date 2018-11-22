801203042e221a7b7d5480c802081d56abe40d76
Hamza Demir <hamzademir4772@hotmail.com>
2018-11-22 20:32:02 +0000

    Add Menu

--- src/main/java/com/hamza/contacts/Application.java
+++ src/main/java/com/hamza/contacts/Application.java
@@ -1,7 +1,31 @@
 package com.hamza.contacts;
 
+import java.util.Scanner;
+
 public class Application{
     public static void main(String[] args){
-        System.out.println("Starting ...");
+
+        ask();
+
+    }
+
+    public static void ask(){
+        System.out.println("Please select an option");
+        System.out.println("1-) List");
+        System.out.println("2-) New Contact");
+        System.out.println("3-) Search");
+        System.out.println("4-) Edit");
+        Scanner scanner=new Scanner(System.in);
+        Integer selectedOption=scanner.nextInt();
+        switch (selectedOption){
+            case 1:list(); break;
+            default:
+                System.out.println("Not available option!");
+                ask();
+        }
+    }
+
+    public static void list(){
+        System.out.println("List of Contacts");
     }
 }
---
+++ src/main/java/com/hamza/contacts/Contact.java
@@ -0,0 +1,15 @@
+package com.hamza.contacts;
+
+public class Contact {
+
+    String firstName;
+    String lastName;
+    String company;
+    String phone;
+    String email;
+    String url;
+    String address;
+    String birthday;
+    String date;
+
+}
